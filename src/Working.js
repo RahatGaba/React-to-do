0696b2e8d873a572109d879f55bf8fb5b387ee88
rahatgaba <rahatgaba89@gmail.com>
2020-06-26 03:24:50 +0500

    Update Working.js

--- src/Working.js
+++ src/Working.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Working(props){
     return <div>
-        <h4>Today, I have eeeeeeeto learn {props.subName}</h4>
+        <h4>Today, I have eeeeeee rahatto learn {props.subName}</h4>
         <h4>Today, I have to work on my assignment and submit it till {props.AssgTime}</h4>
        
     </div>
