99a3f97a3278fbe9a4e0d2c2641da7e4ce43c7bf
rahatgaba <rahatgaba89@gmail.com>
2020-06-26 03:00:29 +0500

    Task Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Working from './Working';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+     <div className="container">
+     <div className="App"> 
+     
+       <header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>
+       REACT TO DO
+       </h1>
+       <Working subName="React" AssgTime="Saturday"/>
+      <hr/>
+       <Working subName="Javascript" AssgTime="Sunday"/>
+      <hr/>
+      <Working subName="Android" AssgTime="July 15"/>
+       </header> 
+    </div> 
+   </div>  
+    
   );
 }
 
---
+++ src/Working.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Working(props){
+    return <div>
+        <h4>Today, I have to learn {props.subName}</h4>
+        <h4>Today, I have to work on my assignment and submit it till {props.AssgTime}</h4>
+       
+    </div>
+     
+    
+}
+export default Working
